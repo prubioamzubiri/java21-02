ccfbe5d886266be16365253a441ad7e0e44461a3
prubioamzubiri <prubioam@zubirimanteo.com>
2022-02-21 14:42:02 +0000

    Proyectos iniciados

---
+++ src/main/java/com/spring2102/spring2102/Spring2102Application.java
@@ -0,0 +1,13 @@
+package com.spring2102.spring2102;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2102Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring2102/spring2102/Spring2102ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring2102.spring2102;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring2102ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
